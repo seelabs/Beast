a6a75c3b6fbf25e26d389737b23b1c49f8a28cf3
Vinnie Falco <vinnie.falco@gmail.com>
2016-07-07 15:47:56 -0400

    [FOLD] test matrix

--- test/core/detail/zlib.cpp
+++ test/core/detail/zlib.cpp
@@ -124,28 +124,34 @@ public:
     {
         static std::size_t constexpr N = 2048;
         auto const original = make_source(N);
-        z_stream zs;
-        zs.zalloc = Z_NULL;
-        zs.zfree = Z_NULL;
-        zs.opaque = Z_NULL;
-        zs.avail_in = 0;
-        zs.next_in = Z_NULL;
-        expect(deflateInit2(&zs, 
-            Z_DEFAULT_COMPRESSION,
-            Z_DEFLATED,
-            -15,
-            4,
-            Z_FIXED) == Z_OK);
-        buffer output(deflateBound(&zs, original.size()));
-        zs.next_in = (Bytef*)original.data();
-        zs.avail_in = original.size();
-        zs.next_out = output.data();
-        zs.avail_out = output.capacity();
-        auto result = deflate(&zs, Z_FULL_FLUSH);
-        deflateEnd(&zs);
-        expect(result == Z_OK);
-        output.resize(output.capacity() - zs.avail_out);
-        checkInflate(output, original);
+        for(int level = 0; level <= 9; ++level)
+        {
+            for(int strategy = 0; strategy <= 4; ++strategy)
+            {
+                z_stream zs;
+                zs.zalloc = Z_NULL;
+                zs.zfree = Z_NULL;
+                zs.opaque = Z_NULL;
+                zs.avail_in = 0;
+                zs.next_in = Z_NULL;
+                expect(deflateInit2(&zs, 
+                    level,
+                    Z_DEFLATED,
+                    -15,
+                    4,
+                    strategy) == Z_OK);
+                buffer output(deflateBound(&zs, original.size()));
+                zs.next_in = (Bytef*)original.data();
+                zs.avail_in = original.size();
+                zs.next_out = output.data();
+                zs.avail_out = output.capacity();
+                auto result = deflate(&zs, Z_FULL_FLUSH);
+                deflateEnd(&zs);
+                expect(result == Z_OK);
+                output.resize(output.capacity() - zs.avail_out);
+                checkInflate(output, original);
+            }
+        }
     }
 
     void run() override
