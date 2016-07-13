0bdc7db4491599d4323fd1baa416c2cefcfbe28d
Vinnie Falco <vinnie.falco@gmail.com>
2016-07-13 15:42:04 -0400

    [FOLD] bits

--- include/beast/core/detail/zlib/inflate.cpp
+++ include/beast/core/detail/zlib/inflate.cpp
@@ -180,23 +180,11 @@ local int updatewindow(
 
 /* Macros for inflate(): */
 
-/* Load registers with state in inflate() for speed */
-#define LOAD() \
-    do { \
-        bits = strm->bits; \
-    } while (0)
-
-/* Restore state from registers in inflate() */
-#define RESTORE() \
-    do { \
-        strm->bits = bits; \
-    } while (0)
-
 /* Clear the input bit accumulator */
 #define INITBITS() \
     do { \
         strm->hold = 0; \
-        bits = 0; \
+        strm->bits = 0; \
     } while (0)
 
 /* Get a byte of input into the bit accumulator, or return from inflate()
@@ -206,16 +194,16 @@ local int updatewindow(
         if (strm->avail_in == 0) goto inf_leave; \
         strm->avail_in--; \
         auto next = reinterpret_cast<std::uint8_t const*>(strm->next_in); \
-        strm->hold += (unsigned long)(*next++) << bits; \
+        strm->hold += (unsigned long)(*next++) << strm->bits; \
         strm->next_in = next; \
-        bits += 8; \
+        strm->bits += 8; \
     } while (0)
 
 /* Assure that there are at least n bits in the bit accumulator.  If there is
    not enough available input to do that, then return from inflate(). */
 #define NEEDBITS(n) \
     do { \
-        while (bits < (unsigned)(n)) \
+        while (strm->bits < (unsigned)(n)) \
             PULLBYTE(); \
     } while (0)
 
@@ -227,127 +215,21 @@ local int updatewindow(
 #define DROPBITS(n) \
     do { \
         strm->hold >>= (n); \
-        bits -= (unsigned)(n); \
+        strm->bits -= (unsigned)(n); \
     } while (0)
 
 /* Remove zero to seven bits as needed to go to a byte boundary */
 #define BYTEBITS() \
     do { \
-        strm->hold >>= bits & 7; \
-        bits -= bits & 7; \
+        strm->hold >>= strm->bits & 7; \
+        strm->bits -= strm->bits & 7; \
     } while (0)
 
-/*
-   inflate() uses a state machine to process as much input data and generate as
-   much output data as possible before returning.  The state machine is
-   structured roughly as follows:
-
-    for (;;) switch (state) {
-    ...
-    case STATEn:
-        if (not enough input data or output space to make progress)
-            return;
-        ... make progress ...
-        state = STATEm;
-        break;
-    ...
-    }
-
-   so when inflate() is called again, the same case is attempted again, and
-   if the appropriate resources are provided, the machine proceeds to the
-   next state.  The NEEDBITS() macro is usually the way the state evaluates
-   whether it can proceed or should return.  NEEDBITS() does the return if
-   the requested bits are not available.  The typical use of the BITS macros
-   is:
-
-        NEEDBITS(n);
-        ... do something with BITS(n) ...
-        DROPBITS(n);
-
-   where NEEDBITS(n) either returns from inflate() if there isn't enough
-   input left to load n bits into the accumulator, or it continues.  BITS(n)
-   gives the low n bits in the accumulator.  When done, DROPBITS(n) drops
-   the low n bits off the accumulator.  INITBITS() clears the accumulator
-   and sets the number of available bits to zero.  BYTEBITS() discards just
-   enough bits to put the accumulator on a byte boundary.  After BYTEBITS()
-   and a NEEDBITS(8), then BITS(8) would return the next byte in the stream.
-
-   NEEDBITS(n) uses PULLBYTE() to get an available byte of input, or to return
-   if there is no input available.  The decoding of variable length codes uses
-   PULLBYTE() directly in order to pull just enough bytes to decode the next
-   code, and no more.
-
-   Some states loop until they get enough input, making sure that enough
-   state information is maintained to continue the loop where it left off
-   if NEEDBITS() returns in the loop.  For example, want, need, and keep
-   would all have to actually be part of the saved state in case NEEDBITS()
-   returns:
-
-    case STATEw:
-        while (want < need) {
-            NEEDBITS(n);
-            keep[want++] = BITS(n);
-            DROPBITS(n);
-        }
-        state = STATEx;
-    case STATEx:
-
-   As shown above, if the next state is also the next case, then the break
-   is omitted.
-
-   A state may also return if there is not enough output space available to
-   complete that state.  Those states are copying stored data, writing a
-   literal byte, and copying a matching string.
-
-   When returning, a "goto inf_leave" is used to update the total counters,
-   update the check value, and determine whether any progress has been made
-   during that inflate() call in order to return the proper return code.
-   Progress is defined as a change in either strm->avail_in or strm->avail_out.
-   When there is a window, goto inf_leave will update the window with the last
-   output written.  If a goto inf_leave occurs in the middle of decompression
-   and there is no window currently, goto inf_leave will create one and copy
-   output to the window for the next call of inflate().
-
-   In this implementation, the flush parameter of inflate() only affects the
-   return code (per zlib.hpp).  inflate() always writes as much as possible to
-   strm->next_out, given the space available and the provided input--the effect
-   documented in zlib.hpp of Z_SYNC_FLUSH.  Furthermore, inflate() always defers
-   the allocation of and copying into a sliding window until necessary, which
-   provides the effect documented in zlib.hpp for Z_FINISH when the entire input
-   stream available.  So the only thing the flush parameter actually does is:
-   when flush is set to Z_FINISH, inflate() cannot return Z_OK.  Instead it
-   will return Z_BUF_ERROR if it has not reached the end of the stream.
- */
-/*
-    State transitions between above modes -
-
-    (most modes can go to BAD or MEM on error -- not shown for clarity)
-
-    Process header:
-        HEAD -> (gzip) or (zlib) or (raw)
-        (gzip) -> FLAGS -> TIME -> OS -> EXLEN -> EXTRA -> NAME -> COMMENT ->
-                  HCRC -> TYPE
-        (zlib) -> DICTID or TYPE
-        DICTID -> DICT -> TYPE
-        (raw) -> TYPEDO
-    Read deflate blocks:
-            TYPE -> TYPEDO -> STORED or TABLE or LEN_ or CHECK
-            STORED -> COPY_ -> COPY -> TYPE
-            TABLE -> LENLENS -> CODELENS -> LEN_
-            LEN_ -> LEN
-    Read deflate codes in fixed or dynamic block:
-                LEN -> LENEXT or LIT or TYPE
-                LENEXT -> DIST -> DISTEXT -> MATCH -> LEN
-                LIT -> LEN
-    Process trailer:
-        CHECK -> LENGTH -> DONE
- */
 int inflate(
     inflate_stream* strm,
     int flush)
 {
     auto state = strm;
-    unsigned bits;              /* bits in bit buffer */
     unsigned in, out;           /* save starting available input and output */
     unsigned copy;              /* number of stored or match bytes to copy */
     unsigned char *from;    /* where to copy match bytes from */
@@ -363,7 +245,6 @@ int inflate(
         return Z_STREAM_ERROR;
 
     if (strm->mode == TYPE) strm->mode = TYPEDO;      /* skip check */
-    LOAD();
     in = strm->avail_in;
     out = strm->avail_out;
     ret = Z_OK;
@@ -486,7 +367,7 @@ int inflate(
             while (strm->have < strm->nlen + strm->ndist) {
                 for (;;) {
                     here = strm->lencode[BITS(strm->lenbits)];
-                    if ((unsigned)(here.bits) <= bits) break;
+                    if ((unsigned)(here.bits) <= strm->bits) break;
                     PULLBYTE();
                 }
                 if (here.val < 16) {
@@ -569,9 +450,7 @@ int inflate(
             strm->mode = LEN;
         case LEN:
             if (strm->avail_in >= 6 && strm->avail_out >= 258) {
-                RESTORE();
                 inflate_fast(strm, out);
-                LOAD();
                 if (strm->mode == TYPE)
                     strm->back = -1;
                 break;
@@ -579,7 +458,7 @@ int inflate(
             strm->back = 0;
             for (;;) {
                 here = strm->lencode[BITS(strm->lenbits)];
-                if ((unsigned)(here.bits) <= bits) break;
+                if ((unsigned)(here.bits) <= strm->bits) break;
                 PULLBYTE();
             }
             if (here.op && (here.op & 0xf0) == 0) {
@@ -587,7 +466,7 @@ int inflate(
                 for (;;) {
                     here = strm->lencode[last.val +
                             (BITS(last.bits + last.op) >> last.bits)];
-                    if ((unsigned)(last.bits + here.bits) <= bits) break;
+                    if ((unsigned)(last.bits + here.bits) <= strm->bits) break;
                     PULLBYTE();
                 }
                 DROPBITS(last.bits);
@@ -629,7 +508,7 @@ int inflate(
         case DIST:
             for (;;) {
                 here = strm->distcode[BITS(strm->distbits)];
-                if ((unsigned)(here.bits) <= bits) break;
+                if ((unsigned)(here.bits) <= strm->bits) break;
                 PULLBYTE();
             }
             if ((here.op & 0xf0) == 0) {
@@ -637,7 +516,7 @@ int inflate(
                 for (;;) {
                     here = strm->distcode[last.val +
                             (BITS(last.bits + last.op) >> last.bits)];
-                    if ((unsigned)(last.bits + here.bits) <= bits) break;
+                    if ((unsigned)(last.bits + here.bits) <= strm->bits) break;
                     PULLBYTE();
                 }
                 DROPBITS(last.bits);
@@ -742,7 +621,6 @@ int inflate(
        Note: a memory error from inflate() is non-recoverable.
      */
   inf_leave:
-    RESTORE();
     if (strm->wsize || (out != strm->avail_out && strm->mode < BAD &&
             (strm->mode < CHECK || flush != Z_FINISH)))
         if (updatewindow(strm, strm->next_out, out - strm->avail_out)) {
